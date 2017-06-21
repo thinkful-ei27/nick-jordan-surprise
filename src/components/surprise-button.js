ee9719bdd0362a82582f802f19901ffdd8b5b151
System <none>
2017-06-21 12:50:07 +0000

    Before import of Thinkful-Ed/react-surprise-starting-point

--- src/components/surprise-button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// Update this component
-export default function SurpriseButton(props) {
-    return <button>Surprise!</button>;
-}
-
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Surprise from './components/surprise';
-import './index.css';
-
-ReactDOM.render(
-  <Surprise />,
-  document.getElementById('root')
-);
