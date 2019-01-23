f85f1f73014c81cb3073b97b2e3d156a631a47ee
Glitch (react-surprise-starting-point) <none>
2019-01-23 09:06:35 -0800

    made button work

--- src/components/surprise-button.js
+++ src/components/surprise-button.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default function SurpriseButton(props) {
-    return <button>Surprise!</button>;
+
+    return <button onClick={(e) => props.handleClick()}>Surprise!</button>;
+    
 }
 
--- src/components/surprise.js
+++ src/components/surprise.js
@@ -6,10 +6,22 @@ import SurpriseImage from './surprise-image';
 export default class Surprise extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            surprise:false
+        }
     }
+        setSurprise(surprise) {
+            event.preventDefault;
+            console.log('showGif ran')
+            this.setState({surprise})
+        }
 
     render() {
         // Show the button to start with
-        return <SurpriseButton />;
+        if (!this.state.surprise) {
+        return <SurpriseButton handleClick={() => this.setSurprise(true)}/>;
+        } else {
+            return <SurpriseImage />;
+        }
     }
 }
